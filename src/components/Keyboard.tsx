61b0387f4cfee9394947d66c909db132811899df
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2022-04-15 18:25:32 -0400

    Added dep to callback

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -27,7 +27,7 @@ function Keyboard({}: Props) {
           }
         })
     }
-  }, [onDelete, onEnter, onSelectLetter])
+  }, [onDelete, onEnter, onSelectLetter, keys])
   
 
   useEffect(() => {
